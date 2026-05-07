3cfe0e3f3f9394d8ae8bd3b1d45adb02a07cbe8b
Priyanshu A <priyanshua@Priyanshus-MacBook-Pro.local>
2026-05-07 12:04:50 +0530

    creates a before each in test for creating instance of Square

--- test/com/tw/bootcamp/problem1/SquareTest.java
+++ test/com/tw/bootcamp/problem1/SquareTest.java
@@ -1,19 +1,23 @@
 package com.tw.bootcamp.problem1;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SquareTest {
+    Square square;
+    @BeforeEach
+    void setup() {
+        square = new Square(2);
+    }
     @Test
     void calculateSquareArea() {
-        Square square = new Square(2);
         assertEquals(4, square.area());
     }
 
     @Test
     void calculateSquarePerimeter() {
-        Square square = new Square(2);
         assertEquals(8, square.perimeter());
     }
 }
