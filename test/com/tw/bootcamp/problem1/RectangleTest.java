e8e929aac8f3d16506f8fbb5d8d973815db90e46
Priyanshu A <priyanshua@Priyanshus-MacBook-Pro.local>
2026-05-07 11:46:21 +0530

    creates a method perimeter in rectangle class and writes a test for it

--- test/com/tw/bootcamp/problem1/RectangleTest.java
+++ test/com/tw/bootcamp/problem1/RectangleTest.java
@@ -1,17 +1,24 @@
 package com.tw.bootcamp.problem1;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 
 class RectangleTest {
-
+    Rectangle rectangle;
+    @BeforeEach
+    void setup() {
+         rectangle = new Rectangle(10, 20);
+    }
     @Test
     void calculatingAreaOfRectangle() {
-        Rectangle rectangle = new Rectangle(10, 20);
-        int area = rectangle.area();
-        assertEquals(200, area);
+        assertEquals(200, rectangle.area());
+    }
+
+    @Test
+    void calculatingPerimeterOfRectangle() {
+        assertEquals(60, rectangle.perimeter());
     }
 }
